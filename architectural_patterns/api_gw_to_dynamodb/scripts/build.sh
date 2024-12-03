9a81d56bc95e7b7e776d7a94a0ff0fb60657d5ce
Wonkyun Yu <hopin133@gmail.com>
2024-12-03 23:57:00 +0900

    feat: update pr actions workflow

--- architectural_patterns/api_gw_to_dynamodb/scripts/build.sh
+++ architectural_patterns/api_gw_to_dynamodb/scripts/build.sh
@@ -3,12 +3,16 @@
 # The following line is used for GitHub Actions --------------
 
 container_name="${1:?Error: No container name provided}"
+optional_param="${2}"
 
-echo "Initiating the container..."
-docker run -dit -v "$(pwd)/../:/app" \
-  --name ${container_name} \
-  ${container_name} /bin/bash
-
+if [[ "${optional_param}" == "actions" ]]; then
+  echo "Initiating the container..."
+  docker run -dit -v "$(pwd)/../:/app" \
+    --name ${container_name} \
+    ${container_name} /bin/bash
+else
+  echo "Skipping container initiation as no 'actions' parameter was provided."
+fi
 # ------------------------------------------------------------
 
 # No build required for this architecture
